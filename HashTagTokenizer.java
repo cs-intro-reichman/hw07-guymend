a813e24e04ae4684d909116e3867a9b1f3ad3cc6
guymend <guy.mendelson@post.runi.ac.il>
2024-01-31 12:09:15 +0200

    hashtagtokenizer

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -4,7 +4,7 @@ public class HashTagTokenizer {
 
 	public static void main(String[] args) {
 
-		String hashTag = args[0];
+		String hashTag = "ilovetheuniversity";
 		String []dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
 	}
@@ -14,13 +14,20 @@ public class HashTagTokenizer {
 
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i = 0; i < 3000; i++) {
+			dictionary[i] = in.readLine();
+		}
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++) {
+			if(dictionary[i].equals(word)){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
@@ -31,9 +38,15 @@ public class HashTagTokenizer {
         }
  
         int N = hashtag.length();
+		String newString = "";
 
         for (int i = 1; i <= N; i++) {
-		
+			newString = hashtag.substring(0,i);
+			if (existInDictionary(newString, dictionary)) {
+				System.out.println(newString);
+				breakHashTag(hashtag.substring(i), dictionary);
+				break;
+			}
         }
     }
 
